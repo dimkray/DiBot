aa3a8d3b28b1d423030d6bfb5ebd8656f52d6fc6
dimkray <dimkray@users.noreply.github.com>
2018-06-09 16:26:53 +0300

    Создание сервиса IATA

---
+++ Services/IATA.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# IATA
+# Key f09869c0-33d9-4f10-8fe3-5467b99492f7
+# https://iatacodes.org/api/VERSION/ENDPOINT?api_key=YOUR-API-KEY&lang=ru
+
+import Fixer
+
+# Код Аэропорта
+def Airport
